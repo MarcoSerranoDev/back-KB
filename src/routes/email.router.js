833404e90f1e5d4b5b0de819cdf0473d6535b553
Andres Cuevas <acg.hermesmusic@gmail.com>
2022-11-24 18:23:43 -0600

    Borrando console de rutas mail

--- src/routes/email.router.js
+++ src/routes/email.router.js
@@ -8,7 +8,6 @@ router.post("/send-email", ctrEmail.sendEmail);
 router.post("/send-me", ctrEmail.sentMeMail);
 
 router.get("/", (req, res) => {
-  console.log("Funciona");
   res.json("funciona");
 });
 
