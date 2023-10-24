913622663c4f429aec411dd190764cecae7987d7
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-10-23 23:53:02 -0500

    ftr: Agregando copy

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -43,13 +43,27 @@ const sentMeMail = async (req, res, next) => {
   const { name, email, message, phone } = req.body;
   try {
     contentHTML = `
-    <div>
-        <h2>${name} se quiere contactar</h2>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Numero:</b> ${phone}</p>
-        <p><b>Mensaje:</b> ${message}</p>
-        
-    </div>
+    <div style="background:#000; padding:30px; color: #FFF; width:100%; max-width:560px" >
+        <img
+          style="width:100%; max-width:500px; margin:auto"
+          src="https://i.ibb.co/PgYJtp0/logo.png"
+          alt="icono kboard"
+        />
+        <h2
+          style="font-size: 20px; margin-bottom: 20px; color: #fff"
+        >
+          ${name} se quiere contactar
+        </h2>
+        <p>
+          <b>Email:</b> ${email}
+        </p>
+        <p>
+          <b>Numero:</b> ${phone}
+        </p>
+        <p>
+          <b>Mensaje:</b> ${message}
+        </p>
+      </div>
   `;
     const transporter = nodemailer.createTransport({
       host: process.env.NODEMAILER_HOST,
@@ -67,7 +81,8 @@ const sentMeMail = async (req, res, next) => {
     let info = await transporter.sendMail({
       from: '"KBoard Music" <info@kboardmusic.com>', // sender address,
       to: "info@kboardmusic.com",
-      subject: "Mensaje contacto",
+      cc: "salvador.gongora@hermes-music.com.mx",
+      subject: "Usuario Kboard quiere contactarse",
       html: contentHTML,
     });
     res.status(200).json(`E-mail sended to me with de id: ${info.messageId}`);
